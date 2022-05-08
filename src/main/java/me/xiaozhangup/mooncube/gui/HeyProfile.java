912dcbb665d5b0f664ac64fb82495ae980b50d3e
xiaozhangup <xiaozhangshiw@163.com>
2022-05-08 20:57:57 +0800

    player profile

---
+++ src/main/java/me/xiaozhangup/mooncube/gui/HeyProfile.java
@@ -0,0 +1,11 @@
+package me.xiaozhangup.mooncube.gui;
+
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+
+public class HeyProfile implements InventoryHolder {
+
+    public Inventory getInventory() {
+        return null;
+    }
+}
---
+++ src/main/java/me/xiaozhangup/mooncube/gui/IsControl.java
@@ -0,0 +1,10 @@
+package me.xiaozhangup.mooncube.gui;
+
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+
+public class IsControl implements InventoryHolder {
+    public Inventory getInventory() {
+        return null;
+    }
+}
