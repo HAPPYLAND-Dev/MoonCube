19112ebb4b8990a125361592418a457a565a8988
xiaozhangup <xiaozhangshiw@163.com>
2022-06-18 14:46:18 +0800

    Remove some items

--- src/main/java/me/xiaozhangup/mooncube/menu/MainMenu.java
+++ src/main/java/me/xiaozhangup/mooncube/menu/MainMenu.java
@@ -33,12 +33,6 @@ public class MainMenu implements Listener {
             }
 
             //TODO
-            menu.setItem(0 , Skull.getSkull("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZTRkNDliYWU5NWM3OTBjM2IxZmY1YjJmMDEwNTJhNzE0ZDYxODU0ODFkNWIxYzg1OTMwYjNmOTlkMjMyMTY3NCJ9fX0=" , "&f显示界面设置" , "&7" , "&7调整游戏的页面控件"));
-            menu.setItem(1 , Skull.getSkull("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOTFjMmY5MjhjNGFiZTMxZTM0MmM4MGM3MWZlZjcyM2U5OTA1NzE3ZjQ5OGRkNzQ2ZWJmOTQxNzk4ODlhNzVjMyJ9fX0=" , "&6鼓励我们" , "&7" , "&7为我们的宣传贴顶贴!" , "&7这样我们可以获得更多曝光"));
-            menu.setItem(2, Skull.getSkull("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYTU4NjlmZDYzNTZkYjA3M2JhZGFlNzZkMTQzNTVkZjM1NGI5NzZjOWExMWIwNjMxZWY3NDc4ZTgyNmRhNTE5MCJ9fX0=" , "&a回到岛屿" , "&7" , "&7传送回自己的岛屿"));
-            menu.setItem(3, Skull.getSkull("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMjUyNTU5ZjJiY2VhZDk4M2Y0YjY1NjFjMmI1ZjJiNTg4ZjBkNjExNmQ0NDY2NmNlZmYxMjAyMDc5ZDI3Y2E3NCJ9fX0=" , "&b服务器负载" , "&7" , "Dev...."));
-            menu.setItem(4, Skull.getSkull("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYjMwMWRmOTkwMjU4NDlmZDk1OTFhNzY5NTJiZjRkMDEzOGU5ZGZiMTQ4ZWM5NDM0N2RlYWRkNTJmNGVjODY2NCJ9fX0=" , "&e宠物管理" , "&7" , "&7管理你的宠物"));
-
             menu.setItem(8 , Skull.getSkull("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOTVkYjg0OWExNWVmMDFhMDMyMTNkMzZjN2M2MmUwN2I4OGMyNzdkYmE4NjkzMTdiN2UyNzU3MWZkZGIyOTNkNCJ9fX0=" , "&x&C&D&8&5&3&F主页面" , "&7" , "&7使用Shift + F打开本菜单" , "&7或者使用 /menu"));
 
             Bukkit.getScheduler().runTask(Main.plugin, () -> p.openInventory(menu));
