10bee19bb18c3de93a9250925104314cb7d94f9a
xiaozhangup <xiaozhangshiw@163.com>
2022-07-08 23:01:30 +0800

    Remove

--- src/main/java/me/xiaozhangup/mooncube/world/RuleManager.java
+++ src/main/java/me/xiaozhangup/mooncube/world/RuleManager.java
@@ -3,6 +3,7 @@ package me.xiaozhangup.mooncube.world;
 import me.xiaozhangup.mooncube.MoonCube;
 import org.bukkit.Bukkit;
 import org.bukkit.GameRule;
+import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
@@ -16,15 +17,6 @@ public class RuleManager implements Listener {
     public void onWorldload(WorldLoadEvent e) {
         e.getWorld().setGameRule(GameRule.KEEP_INVENTORY , true);
         MoonCube.plugin.getLogger().info("World: " + e.getWorld().getName() + " Done!");
-    }
-
-    @EventHandler
-    public void onBlockBreak(BlockDropItemEvent e) {
-        if (e.getPlayer().getWorld().getName().equals("VoidWorld")) {
-            for (Item item : e.getItems()) {
-                item.teleport(e.getPlayer().getLocation());
-            }
-        }
     }
 
     public static void setAll() {
